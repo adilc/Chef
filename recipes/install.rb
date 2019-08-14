721af2451d8d57058c65f0fde40eb99da162c2db
Adil Can <adil.can@Can-FVFY328UHV29.local>
2019-08-14 14:14:49 -0700

    2341

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,6 +1,6 @@
 powershell_script 'Install IIS' do
     code 'Install-WindowsFeature Web-Server'
-    not_if "(Get-WindowsFeature -Name Web-Server".Installed"
+    not_if "(Get-WindowsFeature -Name Web-Server).Installed"
 end
 
 service 'w3svc' do
